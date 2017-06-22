8a89c9e89bb82446871d08404dba1add7a484370
Abdon Megale <abdon.megale@reservafacil.tur.br>
2017-06-22 10:57:51 -0300

    ajustes .sh para gerar maquinas docker e upload dockerhub

--- eureka-server/generate-images.sh
+++ eureka-server/generate-images.sh
@@ -1,2 +1,8 @@
-sudo rm src/main/docker/eureka-server-0.0.1-SNAPSHOT.jar
-sudo cp target/eureka-server-0.0.1-SNAPSHOT.jar src/main/docker/eureka-server-0.0.1-SNAPSHOT.jar
+sudo rm src/main/docker/netflixoss-eureka-0.0.1-SNAPSHOT.jar
+sudo cp target/netflixoss-eureka-0.0.1-SNAPSHOT.jar src/main/docker/netflixoss-eureka-0.0.1-SNAPSHOT.jar
+sudo docker rmi bidonmegale/eukica-machine
+sudo docker build -t bidonmegale/eukica-machine src/main/docker
+sudo docker login
+sudo docker tag bidonmegale/eukica-machine bidonmegale/eukica-machine:latest
+sudo docker push bidonmegale/eukica-machine:latest
+
