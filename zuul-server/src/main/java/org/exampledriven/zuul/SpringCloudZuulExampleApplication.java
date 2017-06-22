e5b97f028bb140d25b921a42a5ca9fb5c47e6d46
miguel <miguel.rapolli@reservafacil.tur.br>
2017-06-22 09:54:58 -0300

    oi

--- zuul-server/src/main/java/org/exampledriven/zuul/SpringCloudZuulExampleApplication.java
+++ zuul-server/src/main/java/org/exampledriven/zuul/SpringCloudZuulExampleApplication.java
@@ -2,19 +2,15 @@ package org.exampledriven.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.UrlResource;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 
 @EnableZuulProxy
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringCloudZuulExampleApplication {
 
     public static void main(String[] args) throws MalformedURLException {
